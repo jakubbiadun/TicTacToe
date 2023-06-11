b7a80d9166e5acabd546432eec9742611cdce8ac
KuaLion <jakubbiadun4@gmail.com>
2023-06-11 14:32:32 +0200

    "Do poprawy zmienianie ikonki i wiadomosc o zakonczeniu gry"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,145 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox> // https://doc.qt.io/qt-6/qmessagebox.html
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    prepareBoard();
+   /* QObject::connect(ui->P1, SIGNAL(clicked()),this, SLOT(P1()));
+    QObject::connect(ui->P2, SIGNAL(clicked()),this, SLOT(P2()));
+    QObject::connect(ui->P3, SIGNAL(clicked()),this, SLOT(P3()));
+    QObject::connect(ui->P4, SIGNAL(clicked()),this, SLOT(P4()));
+    QObject::connect(ui->P5, SIGNAL(clicked()),this, SLOT(P5()));
+    QObject::connect(ui->P6, SIGNAL(clicked()),this, SLOT(P6()));
+    QObject::connect(ui->P7, SIGNAL(clicked()),this, SLOT(P7()));
+    QObject::connect(ui->P8, SIGNAL(clicked()),this, SLOT(P8()));
+    QObject::connect(ui->P9, SIGNAL(clicked()),this, SLOT(P9())); */
+    QObject::connect(ui->newgame, SIGNAL(clicked()),this, SLOT(newGameClicked()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::prepareBoard()
+{
+    player1Turn = 1;
+    amountOfTurns = 0;
+    button[0] = ui ->P1;
+    button[1] = ui ->P2;
+    button[2] = ui ->P3;
+    button[3] = ui ->P4;
+    button[4] = ui ->P5;
+    button[5] = ui ->P6;
+    button[6] = ui ->P7;
+    button[7] = ui ->P8;
+    button[8] = ui ->P9;
+
+    for(int i =0; i<9;i++){
+        connect(button[i],SIGNAL(clicked()), this, SLOT(buttonClicked()));
+        button[i] -> setEnabled(1);
+    }
+    clear();
+    changeTurnLabel();
+}
+
+void MainWindow::isWin()
+{
+    char currentPlayer;
+    if(player1Turn == 1){
+        currentPlayer = 'x';
+    }
+    else currentPlayer = 'o';
+
+    if((board[0] == currentPlayer && board[1] == currentPlayer && board[2] == currentPlayer) ||
+            (board[3] == currentPlayer && board[4] == currentPlayer && board[5] == currentPlayer) ||
+            (board[6] == currentPlayer && board[7] == currentPlayer && board[8] == currentPlayer) ||
+            (board[1] == currentPlayer && board[4] == currentPlayer && board[7] == currentPlayer) ||
+            (board[2] == currentPlayer && board[5] == currentPlayer && board[8] == currentPlayer) ||
+            (board[0] == currentPlayer && board[1] == currentPlayer && board[6] == currentPlayer) ||
+            (board[0] == currentPlayer && board[4] == currentPlayer && board[8] == currentPlayer) ||
+            (board[2] == currentPlayer && board[4] == currentPlayer && board[6] == currentPlayer)){
+                QMessageBox::information(this,"Wygrywa gracz" + QString(currentPlayer));
+                turnOffButton();
+    }
+    else if(amountOfTurns == 9){ // cale pole zapelnione
+        QMessageBox::information(this,"Remis");
+        turnOffButton();
+    }
+}
+
+void MainWindow::clear()
+{
+    for(int i= 0;i<9;i++){
+        button[i]->setIcon(QIcon());
+        button[i]->setText("");
+        board[i] = ' ';
+    }
+}
+
+void MainWindow::turnOffButton()
+{
+    for(int i= 0;i<9;i++){
+        button[i]->setEnabled(0);
+    }
+}
+
+void MainWindow::changeTurnLabel()
+{
+    QLabel *buttonl = ui->TurnIcon;
+    QPixmap pixmap;
+    if(player1Turn == 1){
+        //pixmap(":/img/xsmall.bmp");
+        QPixmap pixmap(":/img/xsmall.bmp");
+    }
+    else{
+        QPixmap pixmap(":/img/osmall.bmp");
+    }
+//    QIcon icon(pixmap);
+//    buttonl->setIcon(icon);
+//    buttonl->setIconSize(pixmap.size());
+}
+
+void MainWindow::buttonClicked()
+{
+    QPushButton *buttonn = qobject_cast<QPushButton*>(sender());
+    int bIndex = -1;
+
+    for(int i=0;i<9;i++){
+        if(button[i] == buttonn){
+            bIndex = i;
+            break;
+        }
+    }
+
+    if(bIndex != -1 && board[bIndex] == ' '){
+        if(player1Turn){
+            QPixmap pixmap(":/img/x.bmp");
+            QIcon icon(pixmap);
+            buttonn->setIcon(icon);
+            buttonn->setIconSize(pixmap.rect().size());
+            board[bIndex] = 'x';
+        }
+        else{
+            QPixmap pixmap(":/img/o.bmp");
+            QIcon icon(pixmap);
+            buttonn->setIcon(icon);
+            buttonn->setIconSize(pixmap.rect().size());
+            board[bIndex] = 'o';
+        }
+    }
+    amountOfTurns++;
+    isWin();
+    player1Turn = !(player1Turn);
+    changeTurnLabel();
+}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QLabel>
+#include <QToolButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+
 private:
     Ui::MainWindow *ui;
+    QPushButton *button[9];
+    QToolButton *turnLabel;
+    bool player1Turn;
+    int amountOfTurns;
+    char board[9];
+
+    void prepareBoard();
+    void isWin();
+    void clear();
+    void turnOffButton();
+    void changeTurnLabel();
+
+private slots:
+   void buttonClicked();
+   void newGameClicked();
+
+
 };
 #endif // MAINWINDOW_H
